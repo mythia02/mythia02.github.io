7a2efd94ddcfb2d796e65913299075b314457de6
Mythia02 <72504379+mythia02@users.noreply.github.com>
2024-10-29 07:53:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*document.addEventListener('click',()=>{
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.querySelector('p').innerHTML = 'SELAMAT TINGGAL!';
+    document.addEventListener('click',()=>{
+        window.close();
+    })
+})*/
